0c6af0c3ecdd29b5531dbae431a081474af78aac
Kevin Lin <klin2358@gmail.com>
2021-08-17 21:20:16 -0400

    created item spawn and power

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Player = require("./scripts/player");
 const Platforms = require("./scripts/platform");
 const Controller = require("./scripts/controller");
 const Zombie = require("./scripts/zombie");
+const Item = require("./scripts/item")
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -16,6 +18,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let zomFrames = 0;
     let multiFrames = 0;
     let player = new Player(canvasWidth, canvasHeight);
+    let item1 = new Item();
+    let item2 = new Item();
+
     // constructor(x,y,width,height,color)
     let platform1 = new Platforms(0, 470, 1000, 30, 'black');
     
@@ -28,13 +33,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let platform6 = new Platforms(666, 225, 333, 10, 'red');
     
     //  Zombie constructor            x          ,  y  ,  width, height, speed, direction, platformX  , platformXW
-    let startZombie = new Zombie(0               ,  0  ,  25   , 470   , 0.5  , "right"  , platform1.x, platform1.xw);
+    // let startZombie = new Zombie(0               ,  0  ,  25   , 470   , 0.5  , "right"  , platform1.x, platform1.xw);
     let zombie      = new Zombie(randomX(0,400)  ,  420,  25   , 50    , 1.3  , "right"  , platform1.x, platform1.xw);
     let zombie1     = new Zombie(randomX(500,975),  420,  25   , 50    , 0.5  , "left"   , platform1.x, platform1.xw);
     let zombie2     = new Zombie(randomX(350,975),  330,  25   , 50    , 3.2  , "left"   , platform3.x, platform3.xw);
     let zombie3     = new Zombie(randomX(0,308)  ,  330,  25   , 50    , 0.9  , "right"  , platform2.x, platform2.xw);
     let zombie4     = new Zombie(randomX(0,308)  ,  175,  25   , 50    , 2    , "left"   , platform5.x, platform5.xw);
     let zombie5     = new Zombie(randomX(350,975),  175,  25   , 50    , 2    , "left"   , platform6.x, platform6.xw);
+    let zombie6     = new Zombie(randomX(325,415),  250,  25   , 50    , 2    , "left"   , platform4.x, platform4.xw);
+
     new Controller(player);
 
     requestAnimationFrame(loop);
@@ -42,6 +49,10 @@ document.addEventListener("DOMContentLoaded", () => {
     function loop(){
 
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        item1.drawItem(ctx);
+        item2.drawItem(ctx);
+
         platform1.drawPlatforms(ctx);
         platform2.drawPlatforms(ctx);
         platform3.drawPlatforms(ctx);
@@ -50,8 +61,8 @@ document.addEventListener("DOMContentLoaded", () => {
         platform6.drawPlatforms(ctx);
         
         fastZom();
-        startZombie.moveZombie(canvasWidth);
-        startZombie.drawZombie(ctx);
+        // startZombie.moveZombie(canvasWidth);
+        // startZombie.drawZombie(ctx);
         
         zombie.moveZombie();
         zombie.drawZombie(ctx);
@@ -71,6 +82,9 @@ document.addEventListener("DOMContentLoaded", () => {
         zombie5.moveZombie();
         zombie5.drawZombie(ctx);
 
+        zombie6.moveZombie();
+        zombie6.drawZombie(ctx);
+
         player.movePlayer();
         player.drawPlayer(ctx);
         
@@ -79,13 +93,16 @@ document.addEventListener("DOMContentLoaded", () => {
         drawScore();
         drawMulti();
 
-        collisionDetection(player, startZombie);
+        // collisionDetection(player, startZombie);
         collisionDetection(player, zombie);
         collisionDetection(player, zombie1);
         collisionDetection(player, zombie2);
         collisionDetection(player, zombie3);
         collisionDetection(player, zombie4);
         collisionDetection(player, zombie5);
+        collisionDetection(player, zombie6);
+        collisionDetection(player, item1);
+        collisionDetection(player, item2);
 
 
         collisionDetection(player, platform1); 
@@ -101,7 +118,6 @@ document.addEventListener("DOMContentLoaded", () => {
     //problem with collision with randomizeZombie
     
     function collisionDetection(obj1, obj2){
-
         // collision event between player, zombie
         if(obj2.constructor === Zombie){
             if( obj1.x < obj2.x + obj2.width &&   
@@ -122,6 +138,46 @@ document.addEventListener("DOMContentLoaded", () => {
              ){
                 obj1.ground = obj2.y - obj1.height + 0.1;
              }
+        }
+
+        if(obj2.constructor === Item){
+            if( 
+                obj1.x < obj2.x + obj2.width &&   
+                obj1.x + obj1.width > obj2.x &&  
+                obj1.y < obj2.y + obj2.height && 
+                obj1.y + obj1.height > obj2.y    
+             ){
+                if(obj2.name === 'mult2'){
+                    multi += 0.2;
+                    console.log("multi2");
+                    itemRespawn(obj2);
+                }
+                else if(obj2.name === 'mult3'){
+                    multi += 0.3;
+                    console.log("multi3");
+                    itemRespawn(obj2);
+                }
+                else if(obj2.name === 'clock'){
+                    slowZom();
+                    console.log("clock");
+                    itemRespawn(obj2);
+                }
+                else if(obj2.name === '+1'){
+                    score += 1
+                    console.log("+1");
+                    itemRespawn(obj2);
+                }
+                else if(obj2.name === '+5'){
+                    score += 5
+                    console.log("+5");
+                    itemRespawn(obj2);
+                }
+                else if(obj2.name === '+10'){
+                    score += 10
+                    console.log("+10");
+                    itemRespawn(obj2);
+                }
+            }
         }
     }
 
@@ -162,8 +218,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function fastZom(){
-        if(zomFrames == 1000){
-            startZombie.speed += 0.01;
+        if(zomFrames == 500){
+            // startZombie.speed += 0.01;
             zombie.speed      += randomSpeed();
             zombie1.speed     += randomSpeed();
             zombie2.speed     += randomSpeed();
@@ -176,6 +232,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function slowZom(){
+        // startZombie.speed -= 0.01;
+        zombie.speed      -= 0.5;
+        zombie1.speed     -= 0.5;
+        zombie2.speed     -= 0.5;
+        zombie3.speed     -= 0.5;
+        zombie4.speed     -= 0.5;
+        zombie5.speed     -= 0.5;
+    }
+
+    function itemRespawn(item){
+        item.randomizeSpawn();
+        item.randomizePower();
+    }
+
     function randomSpeed(){
         return Math.random();
     }
@@ -183,4 +254,5 @@ document.addEventListener("DOMContentLoaded", () => {
     function randomX(min,max){
         return Math.random() * (max - min) + min;
     }
+
 });
--- src/scripts/item.js
+++ src/scripts/item.js
@@ -1,20 +1,51 @@
-const items = [
-    {name: 'mult1', x: 0, y: 0, width: 20, height: 20}, // score multi x1.1
-    {name: 'mult2', x: 0, y: 0, width: 20, height: 20}, // score multi x1.2
-    {name: 'mult3', x: 0, y: 0, width: 20, height: 20}, // score multi x1.3
+const itemsInfo = [
+    {name: 'clock', color: 'blue'}, // slow down zombie
+    {name: 'mult2', color: 'red'}, // multi + 0.2
+    {name: 'mult3', color: 'purple'}, // multi + 0.3
+    {name: '+1', color: 'yellow'}, // +1 score
+    {name: '+5', color: 'pink'}, // + 5 score
+    {name: '+10', color: 'grey'}, // +10 score
 ]
-
-
+const xWidth = [100, 150, 200, 250, 300, 650, 700, 750, 800, 850, 900];
+const yHeight = [290, 400, 190, 320];
 class Item {
     constructor(){
+        this.width = 30;
+        this.height = 30;
+        
+        let items = itemsInfo;
+        let ranNum = Math.floor(Math.random() * (items.length));
+        this.randomItem = items[ranNum];
+        this.name = this.randomItem.name;
+        this.color = this.randomItem.color;
 
+        let xh = xWidth;
+        let yh = yHeight;
+        let ranX = Math.floor(Math.random() * (xh.length));
+        let ranY = Math.floor(Math.random() * (yh.length));
+        this.x = xh[ranX]
+        this.y = yh[ranY];
+    }
+    randomizePower(){
+        let items = itemsInfo;
+        let ranNum = Math.floor(Math.random() * (items.length));
+        this.randomItem = items[ranNum];
+        this.name = this.randomItem.name;
+        this.color = this.randomItem.color;
+    }
+    randomizeSpawn(){
+        let xh = xWidth;
+        let yh = yHeight;
+        let ranX = Math.floor(Math.random() * (xh.length));
+        let ranY = Math.floor(Math.random() * (yh.length));
+        this.x = xh[ranX]
+        this.y = yh[ranY];
     }
 
     drawItem(ctx){
-        ctx.fillStyle = 'orange';
+        ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-
 }
 
 module.exports = Item;
