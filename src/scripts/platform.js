9d972fb1e3be06f604acfd7e86ae7694025d02aa
Kevin Lin <klin2358@gmail.com>
2021-08-19 10:30:38 -0400

    changed platform color for visibility

--- src/scripts/platform.js
+++ src/scripts/platform.js
@@ -7,14 +7,9 @@ class Platforms{
         this.y = y;
 
         this.groundImg = new Image();
-        this.groundImg.src = 'src/images/road.png';
-        this.groundWidth = 1920;
+        this.groundImg.src = 'src/images/sand.png';
+        this.groundWidth = 1913;
         this.groundHeight = 139;
-
-        this.platImg = new Image();
-        this.platImg.src = 'src/images/plat.png';
-        this.platWidth = 1912;
-        this.platHeight = 98;
 
         this.xw = this.x + this.width;
     }
@@ -23,18 +18,7 @@ class Platforms{
         // ctx.drawImage(image, 
         // imageX, imageY, platWidth, platHeight, 
         // this.x, this.y, this.width, this.height);
-
-        ctx.drawImage(this.platImg, 
-            0, 0, this.platWidth, this.platHeight, 
-            this.x, this.y, this.width, this.height);
-
-    }
-
-    drawGround(ctx){
-        // ctx.drawImage(image, 
-        // imageX, imageY, platWidth, platHeight, 
-        // this.x, this.y, this.width, this.height);
-
+        
         ctx.drawImage(this.groundImg, 
             0, 0, this.groundWidth, this.groundHeight, 
             this.x, this.y, this.width, this.height);
